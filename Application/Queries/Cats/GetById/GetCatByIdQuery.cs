b0c88173eca07ac1f3a9432682f1502c1b4de3d6
Mohamad Ahmad <mohamad.ahmad@student.nbi-handelsakademin.se>
2023-12-04 00:09:19 +0100

    Implementera GetAll och GetById för fåglar och katter. För att möjliggöra GetAll och GetById för både fåglar och katter från databas

---
+++ Application/Queries/Cats/GetById/GetCatByIdQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Domain.Models;
+
+namespace Application.Queries.Cats.GetCatById
+{
+    public class GetCatByIdQuery : IRequest<Cat>
+    {
+        public Guid Id { get; }
+
+        public GetCatByIdQuery(Guid catId)
+        {
+            Id = catId;            
+        }
+    }
+}
+
+
