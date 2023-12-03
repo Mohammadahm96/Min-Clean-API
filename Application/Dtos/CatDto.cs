74663aa34557f3eadc9aa20638a0f21961c701fa
Mohamad Ahmad <mohamad.ahmad@student.nbi-handelsakademin.se>
2023-12-03 05:55:36 +0100

    Skapat nya animals. Cat and Bird. och BirdDto samt CatDto

--- Application/Dtos/CatDto.cs
+++ Application/Dtos/CatDto.cs
@@ -1,7 +1,10 @@
 ﻿namespace Application.Dtos
 {
-    public class DogDto
+    public class CatDto
     {
         public string Name { get; set; } = string.Empty;
+        public bool LikesToPlay { get; set; }
+
+
     }
 }
--- Infrastructure/Database/MockDatabase.cs
+++ Infrastructure/Database/MockDatabase.cs
@@ -4,18 +4,23 @@ namespace Infrastructure.Database
 {
     public class MockDatabase
     {
-        public List<Dog> Dogs
-        {
-            get { return allDogs; }
-            set { allDogs = value; }
-        }
-
-        private static List<Dog> allDogs = new()
+        private static List<Dog> allDogs = new List<Dog>
         {
             new Dog { Id = Guid.NewGuid(), Name = "Björn"},
             new Dog { Id = Guid.NewGuid(), Name = "Patrik"},
             new Dog { Id = Guid.NewGuid(), Name = "Alfred"},
             new Dog { Id = new Guid("12345678-1234-5678-1234-567812345678"), Name = "TestDogForUnitTests"}
         };
+
+        public List<Dog> Dogs
+        {
+            get { return allDogs; }
+            set { allDogs = value; }
+        }
+
+
+
     }
 }
+   
+
