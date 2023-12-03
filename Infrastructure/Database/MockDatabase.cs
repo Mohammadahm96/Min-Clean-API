32711c1fd7630d835c964b79fa162055f1fe13f1
Mohamad Ahmad <mohamad.ahmad@student.nbi-handelsakademin.se>
2023-12-03 07:07:48 +0100

    Uppdaterat MockDatabase att innehålla Cat and Bird

--- Infrastructure/Database/MockDatabase.cs
+++ Infrastructure/Database/MockDatabase.cs
@@ -18,7 +18,37 @@ namespace Infrastructure.Database
             set { allDogs = value; }
         }
 
+        private static List<Cat> allCats = new List<Cat>
+        {
+            new Cat { Id = Guid.NewGuid(), Name = "Bulle", LikesToPlay = true },
+            new Cat { Id = Guid.NewGuid(), Name = "Mulle", LikesToPlay = false },
+            new Cat { Id = Guid.NewGuid(), Name = "Hans", LikesToPlay= false }, 
+            new Cat { Id = Guid.NewGuid(), Name = "Musse", LikesToPlay = true },
+            new Cat { Id = new Guid("12345678-1234-5678-1234-567812345679"), Name = "TestCatForUnitTests", LikesToPlay = true }
 
 
+        };
+
+        public List<Cat> Cats
+        {
+            get { return allCats; }
+            set { allCats = value; }
+        }
+
+        private static List<Bird> allBirds = new List<Bird>
+        {
+            new Bird { Id = Guid.NewGuid(), Name = "Tweety", CanFly = true },
+            new Bird { Id = Guid.NewGuid(), Name = "Polly", CanFly = false },
+            new Bird { Id = Guid.NewGuid(), Name = "Mattias", CanFly = true},
+            new Bird { Id = Guid.NewGuid(), Name = "Fly", CanFly= true },
+            new Bird { Id = new Guid("12345678-1234-5678-1234-567812345680"), Name = "TestBirdForUnitTests", CanFly = true }
+        };
+
+        public List<Bird> Birds
+        {
+            get { return allBirds; }
+            set { allBirds = value; }
+        }
     }
 }
+
