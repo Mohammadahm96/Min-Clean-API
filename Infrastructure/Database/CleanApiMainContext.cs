93db8db6c20ddcec1c144e2a61280b5a610f980d
Mohamad Ahmad <mohamad.ahmad@student.nbi-handelsakademin.se>
2023-12-11 13:49:40 +0100

    fix format

--- Infrastructure/Database/CleanApiMainContext.cs
+++ Infrastructure/Database/CleanApiMainContext.cs
@@ -8,15 +8,13 @@ namespace Infrastructure.Database
     public class CleanApiMainContext : DbContext
     {
         public CleanApiMainContext() { }
-        
         public CleanApiMainContext(DbContextOptions<CleanApiMainContext> options) : base(options) { }
 
 
         public virtual DbSet<Dog> Dogs { get; set; } // DbSet for the User table
         public virtual DbSet<Bird> Birds { get; set; }
         public virtual DbSet<Cat>  Cats { get; set; }
-
-        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string connectionString = "Server=localhost;Port=3306;Database=Cleandb;User=root;Password=12345;";
             optionsBuilder.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 32)));
